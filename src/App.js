ddbd24adcb35baee308880c96dae30ba54100a9c
umairx97 <umair.bajwa@hotmail.com>
2019-02-01 22:40:14 +0500

    Added React Router #5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-import Product from "./components/Product";
 import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
 import Details from "./components/Details";
 import Default from "./components/default";
-
-
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
-        <ProductList />
-        <Details />
-        <Cart />
-        <Default />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
       </React.Fragment>
     );
   }
