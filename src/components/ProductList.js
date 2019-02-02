73eb56f4730dd3bec47224fbcb669f420a22f7f8
umairx97 <umair.bajwa@hotmail.com>
2019-02-02 10:40:22 +0500

    Product List init #9

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
-import Product from './Product';
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+
 export default class ProductList extends Component {
+  state = {
+    products: []
+  };
+
   render() {
     return (
-      <div>
-       <Product/>
-      </div>
-    )
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="Our" title="Products" />
+            <div className="row" />
+          </div>
+        </div>
+      </React.Fragment>
+    );
   }
 }
