f043730b33efb5efb2fbfac4edf84cd5cf66d54c
umairx97 <umair.bajwa@hotmail.com>
2019-02-04 20:42:19 +0500

    Intro to context API #10

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import {storeProducts} from '../data'; 
+
+
 
 export default class ProductList extends Component {
   state = {
-    products: []
+    products: storeProducts
   };
 
   render() {
+    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
