dede97d04e42477e688762e0a80af856888b96c1
umairx97 <umair.bajwa@hotmail.com>
2019-02-12 18:52:38 +0500

    Empty Cart Component #26

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
 import Title from "../Title";
 import CartColumns from "./CartColumns";
+import EmptyCart from "./EmptyCart";
+
+import { ProductConsumer } from "../../context";
 
 export default class Cart extends Component {
   render() {
     return (
       <section>
-        <Title name="Your" title="Cart" />
-        <CartColumns />
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name="Your" title="Cart" />
+                  <CartColumns />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
       </section>
     );
   }
