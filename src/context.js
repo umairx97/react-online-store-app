be92651b377d43b6bf61ccda3b371ef51ebf16b8
umairx97 <umair.bajwa@hotmail.com>
2019-02-13 18:50:25 +0500

    Remove Item Functionality #32

--- src/context.js
+++ src/context.js
@@ -89,7 +89,31 @@ class ProductProvider extends Component {
   };
 
   removeItem = id => {
-    console.log("Item Removed");
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+
+    tempCart = tempCart.filter(item => {
+      return item.id !== id;
+    });
+
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          producs: [...tempProducts]
+        };
+      },
+      () => {
+        this.addTotals();
+      }
+    );
   };
 
   clearCart = () => {
