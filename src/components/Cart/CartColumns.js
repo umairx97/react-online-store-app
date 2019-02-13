a0ea4901e62f0c0732c538973e1f7e1e022e6c27
umairx97 <umair.bajwa@hotmail.com>
2019-02-13 17:53:37 +0500

    Cart Items Component #28

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -5,7 +5,7 @@ export default function CartColumns() {
     <div className="container-fluid text-center d-none d-lg-block">
       <div className="row">
         <div className="col-10 mx-auto col-lg-2">
-          <p className="text-uppercase">products</p>
+          <p className="text-uppercase">Products</p>
         </div>
 
         <div className="col-10 mx-auto col-lg-2">
--- src/components/Title.js
+++ src/components/Title.js
@@ -2,11 +2,13 @@ import React from "react";
 
 const Title = ({ name, title }) => {
   return (
-    <div className="row">
-      <div className="col-10 mx-auto my-2 text-center text-title">
-        <h1 className="text-capitalize font-weight-bold">
-          {name} <strong className="text-blue"> {title}</strong>
-        </h1>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-10 mx-auto my-2 text-center text-title">
+          <h1 className="text-capitalize font-weight-bold">
+            {name} <strong className="text-blue"> {title}</strong>
+          </h1>
+        </div>
       </div>
     </div>
   );
