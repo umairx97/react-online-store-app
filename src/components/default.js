6cb0241d0a5c9b2ae2d174dedf9ba9a60400a110
umairx97 <umair.bajwa@hotmail.com>
2019-02-13 21:33:56 +0500

    Page not found #35

--- src/components/default.js
+++ src/components/default.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Default extends Component {
   render() {
+    console.log(this.props);
     return (
-      <div>
-        <h3>Page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="display-3">404</h1>
+            <h1>error</h1>
+            <h5>Page Not found</h5>
+            <hr style={{ border: "solid 1px black" }} />
+            
+            <h3>
+              The Requestd URL{" "}
+              <span className="text-danger">
+                {this.props.location.pathname}
+              </span>{" "}
+              was not found
+            </h3>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
